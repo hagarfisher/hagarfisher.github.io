bf5100485a78798c1a5e2cb91e140e93ca89af9e
Hagar Fisher <hagarfisher@gmail.com>
2023-04-21 21:37:33 +0300

    adding id to section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,8 +3,7 @@ import SectionHeader from "../SectionHeader/SectionHeader";
 import styles from "./style.module.scss";
 function Section({ children, index, title, link }) {
   return (
-    <div className={styles.container}>
-      <a href={`#${link}`} aria-hidden="true"></a>
+    <div id={link} className={styles.container}>
       <SectionHeader index={index} title={title} />
       {children}
     </div>
